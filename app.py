780ceb7940c6d9a8cdf74dc8b37daf6f9f7a486a
Vanaja <mandhagirivanaja@gmail.com>
2026-07-09 10:19:30 -0400

    fix Render deployment

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
 import joblib
+import os
 
 app = Flask(__name__)
 
+# Load model and vectorizer
 model = joblib.load("model/spam_model.pkl")
 vectorizer = joblib.load("model/vectorizer.pkl")
 
@@ -17,13 +19,12 @@ def predict():
     prediction = model.predict(data)
 
     if prediction[0] == 1:
-        result = "🚨 This is a Spam Email!"
+        result = "⚠️ This is a Spam Email!"
     else:
         result = "✅ This is Not Spam."
 
     return render_template("index.html", prediction=result)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1",
-port=5000,  debug=True)
-   
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
